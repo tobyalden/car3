d563fdb6279c03efb898c2851dc2c8dbda4230c2
Toby Alden <toby.alden@gmail.com>
2023-03-20 22:19:59 -0700

    Add car

--- source/Main.hx
+++ source/Main.hx
@@ -11,6 +11,11 @@ import scenes.*;
 
 class Main extends Engine
 {
+    public static inline var GAMEPAD_LEFT_TRIGGER_AXIS = 4;
+    public static inline var GAMEPAD_RIGHT_TRIGGER_AXIS = 5;
+    public static inline var GAMEPAD_LEFT_BUMPER = 9;
+    public static inline var GAMEPAD_RIGHT_BUMPER = 10;
+
     static function main() {
         new Main();
     }
@@ -22,10 +27,11 @@ class Main extends Engine
         HXP.screen.scaleMode = new UniformScaleMode(UniformScaleType.Expand);
         HXP.fullscreen = true;
 
-        Key.define("up", [Key.W, Key.UP]);
-        Key.define("down", [Key.S, Key.DOWN]);
+        Key.define("forward", [Key.W, Key.UP]);
+        Key.define("reverse", [Key.S, Key.DOWN]);
         Key.define("left", [Key.A, Key.LEFT]);
         Key.define("right", [Key.D, Key.RIGHT]);
+        Key.define("drift", [Key.Z, Key.SPACE]);
 
         if(Gamepad.gamepad(0) != null) {
             defineGamepadInputs(Gamepad.gamepad(0));
@@ -39,12 +45,13 @@ class Main extends Engine
     }
 
     private function defineGamepadInputs(gamepad) {
-        gamepad.defineButton("up", [XboxGamepad.DPAD_UP]);
-        gamepad.defineButton("down", [XboxGamepad.DPAD_DOWN]);
+        gamepad.defineButton("forward", [XboxGamepad.DPAD_UP]);
+        gamepad.defineButton("reverse", [XboxGamepad.DPAD_DOWN]);
         gamepad.defineButton("left", [XboxGamepad.DPAD_LEFT]);
         gamepad.defineButton("right", [XboxGamepad.DPAD_RIGHT]);
-        gamepad.defineAxis("up", XboxGamepad.LEFT_ANALOGUE_Y, -0.5, -1);
-        gamepad.defineAxis("down", XboxGamepad.LEFT_ANALOGUE_Y, 0.5, 1);
+        gamepad.defineButton("drift", [XboxGamepad.A_BUTTON, GAMEPAD_RIGHT_BUMPER]);
+        gamepad.defineAxis("forward", GAMEPAD_RIGHT_TRIGGER_AXIS, 0.5, 1);
+        gamepad.defineAxis("reverse", GAMEPAD_LEFT_TRIGGER_AXIS, 0.5, 1);
         gamepad.defineAxis("left", XboxGamepad.LEFT_ANALOGUE_X, -0.5, -1);
         gamepad.defineAxis("right", XboxGamepad.LEFT_ANALOGUE_X, 0.5, 1);
     }
--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -11,44 +11,169 @@ import scenes.*;
 
 class Player extends Entity
 {
-    public static inline var SPEED = 100;
+    public static inline var ACCEL = 50 * 8;
+    public static inline var MAX_SPEED = 100 * 2;
+    public static inline var TURN_SPEED = 100 * 3;
+    public static inline var POST_DRIFT_BOOST = 3;
+    public static inline var MAX_DRIFT_BOOST_DURATION = 1;
+    public static inline var SOFT_CLAMP_APPROACH_SPEED = ACCEL * 1.75;
+
+    public static var sfx:Map<String, Sfx> = null;
 
     private var sprite:Spritemap;
+    private var hitbox:Polygon;
+    private var angle:Float;
+    private var speed:Float;
+    private var isDrifting:Bool;
     private var velocity:Vector2;
+    private var driftTimer:Float;
 
     public function new(x:Float, y:Float) {
         super(x, y);
-        mask = new Hitbox(10, 10);
-        sprite = new Spritemap("graphics/player.png", 10, 10);
+        name = "player";
+        sprite = new Spritemap("graphics/player.png", 10, 16);
         sprite.add("idle", [0]);
+        sprite.add("drifting", [1]);
         sprite.play("idle");
+        sprite.centerOrigin();
+        hitbox = Polygon.createFromArray([
+            -5, -8,
+            -5, 8,
+            5, 8,
+            5, -8
+        ]);
+        mask = hitbox;
         graphic = sprite;
+        angle = 0;
+        speed = 0;
         velocity = new Vector2();
+        driftTimer = 0;
+        isDrifting = false;
+        if(sfx == null) {
+            sfx = [
+                "die" => new Sfx("audio/die.wav"),
+                "carloop" => new Sfx("audio/carloop.wav"),
+                "drift" => new Sfx("audio/drift.wav"),
+                "boost" => new Sfx("audio/boost.wav")
+            ];
+        }
     }
 
     override public function update() {
-        var heading = new Vector2();
-        if(Input.check("left")) {
-            heading.x = -1;
-        }
-        else if(Input.check("right")) {
-            heading.x = 1;
-        }
-        else {
-            heading.x = 0;
-        }
-        if(Input.check("up")) {
-            heading.y = -1;
-        }
-        else if(Input.check("down")) {
-            heading.y = 1;
-        }
-        else {
-            heading.y = 0;
-        }
-        velocity = heading;
-        velocity.normalize(SPEED);
-        moveBy(velocity.x * HXP.elapsed, velocity.y * HXP.elapsed, ["walls"]);
+        movement();
+        //collisions();
+        animation();
         super.update();
     }
+
+    private function movement() {
+        isDrifting = Input.check("drift");
+        if(!isDrifting && driftTimer > 0) {
+            var driftBoost = Math.min(driftTimer, MAX_DRIFT_BOOST_DURATION) / MAX_DRIFT_BOOST_DURATION;
+            speed *= (1 + driftBoost);
+            sfx["boost"].play(driftBoost);
+            sfx["carloop"].volume = 1;
+        }
+        driftTimer = isDrifting ? driftTimer + HXP.elapsed : 0;
+        var oldAngle = angle;
+        var turnSpeed = isDrifting ? TURN_SPEED * 1.35 : TURN_SPEED;
+        if(Input.check("left")) {
+            angle += turnSpeed * HXP.elapsed;
+        }
+        if(Input.check("right")) {
+            angle -= turnSpeed * HXP.elapsed;
+        }
+        if(Input.check("forward")) {
+            speed += ACCEL * HXP.elapsed;
+        }
+        else if(Input.check("reverse")) {
+            speed -= ACCEL * HXP.elapsed;
+        }
+        else {
+            speed = MathUtil.approach(speed, 0, ACCEL * HXP.elapsed);
+        }
+
+        // Soft clamp speed
+        var maxSpeed = isDrifting ? MAX_SPEED * 1.25 : MAX_SPEED;
+        if(speed > MAX_SPEED) {
+            speed = MathUtil.approach(
+                speed, MAX_SPEED, SOFT_CLAMP_APPROACH_SPEED * HXP.elapsed
+            );
+        }
+        else if(speed < -MAX_SPEED) {
+            speed = MathUtil.approach(
+                speed, -MAX_SPEED, SOFT_CLAMP_APPROACH_SPEED * HXP.elapsed
+            );
+        }
+
+        // Hard clamp speed
+        var hardClamp = MAX_SPEED * POST_DRIFT_BOOST;
+        speed = MathUtil.clamp(speed, -hardClamp, hardClamp);
+
+        var heading = new Vector2();
+        MathUtil.angleXY(heading, angle + 90, speed);
+        if(!isDrifting) {
+            heading.scale(0.75);
+        }
+        else {
+            heading.scale(0.125);
+        }
+        velocity.add(heading);
+        velocity.normalize(Math.abs(speed));
+        moveBy(velocity.x * HXP.elapsed, velocity.y * HXP.elapsed, "walls");
+        if(!sfx["carloop"].playing) {
+            sfx["carloop"].loop();
+        }
+        sfx["carloop"].volume = Math.abs(speed) / MAX_SPEED * (isDrifting ? 0.25 : 1);
+        if(isDrifting) {
+            if(!sfx["drift"].playing) {
+                sfx["drift"].loop();
+            }
+            sfx["drift"].volume = Math.abs(speed) / MAX_SPEED;
+        }
+        else {
+            sfx["drift"].stop();
+        }
+    }
+
+    private function animation() {
+        sprite.play(isDrifting ? "drifting" : "idle");
+        sprite.angle = angle;
+    }
+
+    //private function collisions() {
+        //if(collide("hazard", x, y) != null) {
+            //die();
+        //}
+    //}
+
+    //private function explode() {
+        //var numExplosions = 50;
+        //var directions = new Array<Vector2>();
+        //for(i in 0...numExplosions) {
+            //var angle = (2/numExplosions) * i;
+            //directions.push(new Vector2(Math.cos(angle), Math.sin(angle)));
+            //directions.push(new Vector2(-Math.cos(angle), Math.sin(angle)));
+            //directions.push(new Vector2(Math.cos(angle), -Math.sin(angle)));
+            //directions.push(new Vector2(-Math.cos(angle), -Math.sin(angle)));
+        //}
+        //var count = 0;
+        //for(direction in directions) {
+            //direction.scale(0.8 * Math.random());
+            //direction.normalize(
+                //Math.max(0.1 + 0.2 * Math.random(), direction.length)
+            //);
+            //var explosion = new Particle(
+                //centerX, centerY, directions[count], 1, 1
+            //);
+            //explosion.layer = -99;
+            //scene.add(explosion);
+            //count++;
+        //}
+
+//#if desktop
+        //Sys.sleep(0.02);
+//#end
+        //scene.camera.shake(1, 4);
+    //}
 }
